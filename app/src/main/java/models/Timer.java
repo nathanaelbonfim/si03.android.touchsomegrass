f75b8eb704ac8773487462fceeacfaac4e6025f6
Vinicius Santana <viniciusantana323@gmail.com>
2022-04-15 21:25:02 -0300

    refactor: structure folders

---
+++ app/src/main/java/models/Timer.java
@@ -0,0 +1,50 @@
+package models;
+
+public class Timer {
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getInterval() {
+        return interval;
+    }
+
+    public void setInterval(int interval) {
+        this.interval = interval;
+    }
+
+    public int getTimeToActive() {
+        return timeToActive;
+    }
+
+    public void setTimeToActive(int timeToActive) {
+        this.timeToActive = timeToActive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String message;
+    private int interval;
+    private int timeToActive;
+    private String name;
+
+
+    public Time(String message, int interval, int timeToactive, String message) {
+        this.message = message;
+        this.interval = interval;
+        this.timeToActive = timeToactive;
+        this.message = message;
+    }
+
+
+}
--- app/src/main/java/presenters/timerPresenter.java
+++ app/src/main/java/presenters/timerPresenter.java
@@ -1,3 +1,5 @@
+package presenters;
+
 public interface Timer {
     public int setValue(int value);
     public int getRestain();
