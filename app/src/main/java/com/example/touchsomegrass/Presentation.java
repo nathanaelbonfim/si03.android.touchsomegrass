a1d16206abdcee4ed42807a51d2764e6fb8a6990
Nathanael <nathanael.bonfim@edu.unipar.br>
2022-04-16 17:37:34 -0300

    refactor(activity/configurations): Add input text fields

--- app/src/main/java/com/example/touchsomegrass/Presentation.java
+++ app/src/main/java/com/example/touchsomegrass/Presentation.java
@@ -2,7 +2,9 @@ package com.example.touchsomegrass;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Presentation extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class Presentation extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presentation);
+    }
+
+    public void openConfigurations(View v) {
+        startActivity(new Intent(Presentation.this, Configuration.class));
     }
 }
