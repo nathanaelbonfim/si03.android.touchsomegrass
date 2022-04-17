ec95fed0f9ef4ae7aadf0c271e40d4ed1ecf2846
Vinicius Santana <viniciusantana323@gmail.com>
2022-04-17 16:07:23 -0300

    feat: add listener input

--- app/src/main/java/com/example/touchsomegrass/Configuration.java
+++ app/src/main/java/com/example/touchsomegrass/Configuration.java
@@ -1,15 +1,30 @@
 package com.example.touchsomegrass;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
-public class Configuration extends BaseActivity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-                super.onCreate(savedInstanceState);
-                setContentView(R.layout.activity_configuration);
-        }
+import com.google.android.material.textfield.TextInputLayout;
 
-        public void saveConfigs(View view) {
-        }
+public class Configuration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuration);
+        TextInputLayout inputName = findViewById(R.id.configuration_name);
+        TextInputLayout inputMessage = findViewById(R.id.configuration_message);
+        inputName.addOnEditTextAttachedListener(new TextInputLayout.OnEditTextAttachedListener() {
+            @Override
+            public void onEditTextAttached(@NonNull TextInputLayout textInputLayout) {
+                Log.i("Configuration", "changes");
+            }
+        });
+    }
 }
